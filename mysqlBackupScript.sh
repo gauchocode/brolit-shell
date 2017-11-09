4ab2e29aa562e1562959e28759a7f07242537176
Leandro Nahuel Padula <leandro@broobe.com>
2017-11-09 18:09:50 -0300

    envio de emails en mysql backup

--- mysqlBackupScript.sh
+++ mysqlBackupScript.sh
@@ -8,11 +8,13 @@
 #############################################################################
 
  ### TO EDIT ###
+#VPSNAME="$HOSTNAME"
+VPSNAME="[VPS_NAME]"
 MUSER="[MYSQL_USER]"
 MPASS="[MYSQL_PASSWORD]"
 BAK="/root/tmp"
 SFOLDER="/root/backup-scripts/"
-#VPSNAME="$HOSTNAME"
+MAILA="soporte@broobe.com"
 
 ### MySQL CONFIG ###
 MHOST="localhost"
@@ -56,29 +58,11 @@ cd $SFOLDER
 ./dropbox_uploader.sh upload $FILE /
 ./dropbox_uploader.sh remove /db-$db-$ONEWEEKAGO.gz
 
-#
-#if [ 1 -ne $AMOUNT ]; then
-#STATE="ERROR"
-#SUBJECT="DROBI-VPS [$NOWDISPLAY] - mysqlBackups - ERROR"
-#CONTENT="<b>Ocurrio un error. Los archivos incluidos en el backup, son menos que la cantidad de bases de arqa:</b> <br />"
-#COLOR='red'
-#
-#else
-#STATE="OK"
-#SUBJECT="DROBI-VPS [$NOWDISPLAY] - mysqlBackups - OK"
-#CONTENT="<b>Los archivos incluidos en el backup diario son:</b><br />$BACKUPEDLIST"
-#COLOR='#1DC6DF'
-#fi
-#HEADEROPEN1='<html><body><div style="float:left;width:500px"><div style="font-size:13px;color:#FFF;float:left;font-family: Verdana, Tahoma, Helvetica, Arial;line-height:31px;background:'
-#HEADEROPEN2='; padding-left:5px;width:500px; height:20px">'
-#HEADEROPEN=$HEADEROPEN1$COLOR$HEADEROPEN2
-#HEADERCLOSE='</div>'
-#BODYOPEN='<div style="color:#000;font-size:10px; float:left;font-family: Verdana, Tahoma, Helvetica, Arial;background:#D8D8D8;padding-left:5px;width:500px; height:130px">'
-#BODYCLOSE='</div>'
-#FOOTER='<div style="font-size:10px; float:left;font-family: Verdana, Tahoma, Helvetica, Arial;text-align:right; padding-right:5px;width:500px; height:20px">Broobe Team</div></div></body></html>'
-#
-#HEADER=$HEADEROPEN$SUBJECT$HEADERCLOSE
-#
-#BODY=$BODYOPEN$CONTENT$BODYCLOSE
-#
-#sendEmail -f admin@broobe.com -t "dev@broobe.com" -u "BROOBE-VPS01-BACKUPS [MySQL] - [$STATE] ($NOWDISPLAY)" -m "$HEADER $BODY $FOOTER" -s smtp.gmail.com -o tls=yes -xu admin@broobe.com -xp br00b34dm1n
+### Send Email ###
+if [ 1 -ne $AMOUNT ]; then
+  echo "HUBO UN PROBLEMA CON EL BACKUP EN $VPSNAME" | mailx -v -r "no-reply@send.broobe.com" -a 'Content-Type: text/html' -s "BROOBE: $VPSNAME DB BACKUP --ERROR" -S smtp-use-starttls -S smtp="mx.bmailing.com.ar:587" -S smtp-auth=login -S smtp-auth-user="no-reply@send.broobe.com" -S smtp-auth-password="broobe2020*" -S ssl-verify=ignore $MAILA
+  echo "Backup con errores"
+else
+  echo "BACKUP REALIZADO CON EXITO EN $VPSNAME<br /><br /><b>Los archivos incluidos en el backup diario son:</b><br />$BACKUPEDLIST" | mailx -v -r "no-reply@send.broobe.com" -a 'Content-Type: text/html' -s "BROOBE: $VPSNAME DB BACKUP --OK" -S smtp-use-starttls -S smtp="mx.bmailing.com.ar:587" -S smtp-auth=login -S smtp-auth-user="no-reply@send.broobe.com" -S smtp-auth-password="broobe2020*" -S ssl-verify=ignore $MAILA
+  echo "Backup exitoso"
+fi
