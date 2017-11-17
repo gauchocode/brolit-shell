0ac404323ad8999150b9c836f4d0c2bf5d4b9d58
lpadula <leandro@broobe.com>
2017-11-17 12:31:04 -0300

    bugfixing, mejoras en mails y en duplicity

--- runner.sh
+++ runner.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
- ### TO EDIT ###
+### TO EDIT ###
 SFOLDER="/root/backup-scripts/"
 
  ### RUNNER ###
 $SFOLDER/mysqlBackupScript.sh
 $SFOLDER/filesBackupScript.sh
-
-#cd /root/backup-scripts/
-#./dropbox_uploader.sh upload ./ /$HOSTNAME/
