0d85942c5c4ac29b36a3f3a46cb605e4e2501121
Leandro Padula <leandro@broobe.com>
2019-04-02 18:25:56 -0300

    mejoras en lemp.sh y nuevo wordpress.sh

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -4,12 +4,32 @@
 # Version: 1.1
 #############################################################################
 
-#SERVER_MODEL OPTIONS= cx11, cx21, cx31
-SERVER_MODEL=""
+#conf vars
+SERVER_MODEL=""                               #Options: cx11, cx21, cx31
 DOMAIN="DOMAIN_NAME"
+COMPOSER="false"
+WP="false"
+#MUSER=""              												#MySQL User
+#MPASS=""          														#MySQL User Pass
+
+### Log Start ###
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+PATH_LOG="$SFOLDER/logs"
+if [ ! -d "$SFOLDER/logs" ]
+then
+    echo " > Folder $SFOLDER/logs doesn't exist. Creating now ..."
+    mkdir $SFOLDER/logs
+    echo " > Folder $SFOLDER/logs created ..."
+fi
+
+LOG_NAME=log_lemp_$TIMESTAMP.log
+LOG=$PATH_LOG/$LOG_NAME
+
+### EXPORT VARS ###
+export DOMAIN LOG
 
 #updating packages
-echo -e "\nUpdating package lists..\n"
+echo -e "\nUpdating package lists..\n" >>$LOG
 
 sudo apt --yes install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
@@ -24,22 +44,50 @@ sudo dpkg-reconfigure tzdata
 #secure mysql installation
 sudo mysql_secure_installation
 
-#nginx conf file
-echo -e "\nMoving nginx configuration files...\n"
-#default site configuration
-sudo mv confs/default /etc/nginx/sites-available
-#netdata proxy configuration
-sudo mv confs/monitor /etc/nginx/sites-available
-#nginx.conf broobe standard configuration
-cat confs/nginx.conf > /etc/nginx/nginx.conf
-
 #php.ini broobe standard configuration
-echo -e "\nMoving php configuration file...\n"
+echo -e "\nMoving php configuration file...\n" >>$LOG
 cat confs/php.ini > /etc/php/7.2/fpm/php.ini
 
 #fpm broobe standard configuration
-echo -e "\nMoving fpm configuration file...\n"
+echo -e "\nMoving fpm configuration file...\n" >>$LOG
 cat confs/$SERVER_MODEL/www.conf > /etc/php/7.2/fpm/pool.d/www.conf
+
+#remove html default nginx folders
+rm -r /var/www/html
+
+#nginx.conf broobe standard configuration
+cat confs/nginx.conf > /etc/nginx/nginx.conf
+
+#nginx conf file
+echo -e "\nMoving nginx configuration files...\n" >>$LOG
+#empty default site configuration
+echo " " >> /etc/nginx/sites-available/default
+#netdata proxy configuration
+sudo cp confs/monitor /etc/nginx/sites-available
+
+#new site configuration
+sudo cp confs/default /etc/nginx/sites-available/$DOMAIN
+ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/$DOMAIN
+
+if [ "$WP" = true ] ; then
+  sh wordpress.sh
+fi
+
+if [ "$COMPOSER" = true ] ; then
+  EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+  if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+  then
+      >&2 echo 'ERROR: Invalid installer signature' >>$LOG
+      rm composer-setup.php
+      exit 1
+  fi
+  php composer-setup.php --quiet
+  RESULT=$?
+  rm composer-setup.php
+  exit $RESULT
+fi
 
 #replacing string to match domain name
 #sudo replace "domain.com" "$DOMAIN" -- /etc/nginx/sites-available/default
@@ -53,16 +101,26 @@ sudo sed -i "s#dominio.com#$DOMAIN#" /etc/nginx/sites-available/monitor
 ln -s /etc/nginx/sites-available/monitor /etc/nginx/sites-enabled/monitor
 #ln -s /etc/nginx/sites-available/phpmyadmin /etc/nginx/sites-enabled/phpmyadmin
 
-#fixing a bug?
-mv /run/php/php7.0-fpm.sock /run/php/php7.2-fpm.sock
-
 echo -e "\nRestarting services...\n"
 sudo systemctl restart php7.2-fpm
 sudo systemctl restart nginx.service
 
+#TODO: ya dejar configurada las extensiones de mysql y nginx
 echo -e "\nInstalling Netdata...\n"
 sudo apt --yes install zlib1g-dev uuid-dev libmnl-dev gcc make git autoconf autoconf-archive autogen automake pkg-config curl python-mysqldb
 git clone https://github.com/firehol/netdata.git --depth=1
-cd netdata && ./netdata-installer.sh
+cd netdata && ./netdata-installer.sh --dont-wait
 killall netdata && cp system/netdata.service /etc/systemd/system/
+
+cat confs/netdata/mysql.conf > /usr/lib/netdata/conf.d/python.d/mysql.conf
+#TODO: Falta hacer esto:
+#
+#mysql -u $MUSER -p $MPASS
+#MariaDB [(none)]> CREATE USER 'netdata'@'localhost';
+#MariaDB [(none)]> GRANT USAGE on *.* to 'netdata'@'localhost';
+#MariaDB [(none)]> FLUSH PRIVILEGES;
+#MariaDB [(none)]> exit
+
 systemctl daemon-reload && systemctl enable netdata && service netdata start
+
+echo "Backup :: Script End -- $(date +%Y%m%d_%H%M)" >> $LOG
--- optimizationsScript.sh
+++ optimizationsScript.sh
@@ -6,16 +6,20 @@
 #############################################################################
 
 ### Remove old packages from system ###
+echo " > Clean old system packages..." >> $LOG
 apt clean
 apt-get -y autoremove
 apt-get -y autoclean
 
 ### Remove old log files from system ###
+echo " > Delete old system logs..." >> $LOG
 find /var/log/ -mtime +7 -type f -delete
 
 # Optimización de imágenes (.jpg) para archivos modificados en los últimos 7 días
+echo " > Running jpegoptim..." >> $LOG
 cd $SITES
 find -mtime -7 -type f -name "*.jpg" -exec jpegoptim --max=80 --strip-all {} \;
 
 # Optimización de imágenes (.png) para archivos modificados en los últimos 7 días
+echo " > Running optipng..." >> $LOG
 find -mtime -7 -type f -name "*.png" -exec optipng -o7 -strip all {} \;
