c1c20aa199d2777c6aba1ebb009155156f5a1f00
lpadula <leandro@broobe.com>
2019-08-05 19:20:12 -0300

    nuevo menu de instaladores, y bugfixing

--- restore_from_backup.sh
+++ restore_from_backup.sh
@@ -1,121 +1,12 @@
 #!/bin/bash
 # Autor: broobe. web + mobile development - https://broobe.com
-# Version: 2.9.5
+# Version: 2.9.7
 ################################################################################
 
 # TODO: ARREGLAR RESTORE DE BD! el matching directory es un peligro!
 # y ni hablar de cambiar el wp-config sin checkear los datos del usuario creado.
 
-SCRIPT_V="2.9.5"
-
-startdir=""
-menutitle="Config Selection Menu"
-
-################################# HELPERS ######################################
-
-Filebrowser() {
-  # first parameter is Menu Title
-  # second parameter is dir path to starting folder
-  if [ -z $2 ] ; then
-    dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
-  else
-    cd "$2"
-    dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
-  fi
-  curdir=$(pwd)
-  if [ "$curdir" == "/" ] ; then  # Check if you are at root folder
-    selection=$(whiptail --title "$1" \
-                          --menu "Select a Folder or Tab Key\n$curdir" 0 0 0 \
-                          --cancel-button Cancel \
-                          --ok-button Select $dir_list 3>&1 1>&2 2>&3)
-  else   # Not Root Dir so show ../ BACK Selection in Menu
-    selection=$(whiptail --title "$1" \
-                          --menu "Select a Folder or Tab Key\n$curdir" 0 0 0 \
-                          --cancel-button Cancel \
-                          --ok-button Select ../ BACK $dir_list 3>&1 1>&2 2>&3)
-  fi
-  RET=$?
-  if [ $RET -eq 1 ]; then  # Check if User Selected Cancel
-    return 1
-  elif [ $RET -eq 0 ]; then
-    if [[ -f "$selection" ]]; then  # Check if File Selected
-      if (whiptail --title "Confirm Selection" --yesno "Selection : $selection\n" 0 0 \
-                   --yes-button "Confirm" \
-                   --no-button "Retry"); then
-        filename="$selection"
-        filepath="$curdir"    # Return full filepath and filename as selection variables
-      fi
-    fi
-  fi
-}
-
-Directorybrowser() {
-  # first parameter is Menu Title
-  # second parameter is dir path to starting folder
-
-  if [ -z $2 ] ; then
-    dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
-  else
-    cd "$2"
-    dir_list=$(ls -lhp  | awk -F ' ' ' { print $9 " " $5 } ')
-  fi
-  curdir=$(pwd)
-  if [ "$curdir" == "/" ] ; then  # Check if you are at root folder
-    selection=$(whiptail --title "$1" \
-                          --menu "Select a Folder or Tab Key\n$curdir" 0 0 0 \
-                          --cancel-button Cancel \
-                          --ok-button Select $dir_list 3>&1 1>&2 2>&3)
-  else   # Not Root Dir so show ../ BACK Selection in Menu
-    selection=$(whiptail --title "$1" \
-                          --menu "Select a Folder or Tab Key\n$curdir" 0 0 0 \
-                          --cancel-button Cancel \
-                          --ok-button Select ../ BACK $dir_list 3>&1 1>&2 2>&3)
-  fi
-  RET=$?
-  if [ $RET -eq 1 ]; then  # Check if User Selected Cancel
-    return 1
-  elif [ $RET -eq 0 ]; then
-    if [[ -d "$selection" ]]; then  # Check if Directory Selected
-      if (whiptail --title "Confirm Selection" --yesno "Selection : $selection\n" 0 0 \
-                   --yes-button "Confirm" \
-                   --no-button "Retry"); then
-        filename="$selection"
-        filepath="$curdir"    # Return full filepath and filename as selection variables
-
-      fi
-    fi
-  fi
-}
-
-ChooseProjectState() {
-  PROJECT_STATES="prod stage test dev"
-  PROJECT_STATE=$(whiptail --title "PROJECT STATE" --menu "Chose a Project State" 20 78 10 `for x in ${PROJECT_STATES}; do echo "$x [X]"; done` 3>&1 1>&2 2>&3)
-  exitstatus=$?
-  if [ $exitstatus = 0 ]; then
-    echo -e ${YELLOW}"Project state selected: ${PROJECT_STATE} ..."${ENDCOLOR}
-
-  else
-    exit 1
-  fi
-}
-
-FolderToRestore() {
-  if [[ -z "${FOLDER_TO_RESTORE}" ]]; then
-    FOLDER_TO_RESTORE=$(whiptail --title "Folder to Restore Backup" --inputbox "Please insert a folder to restore the backup files." 10 60 "/var/www" 3>&1 1>&2 2>&3)
-    exitstatus=$?
-    if [ $exitstatus = 0 ]; then
-      echo "FOLDER_TO_RESTORE="${FOLDER_TO_RESTORE} >> $LOG
-    else
-      exit 0
-    fi
-  fi
-}
-
-################################################################################
-
-SITES_F="sites"
-CONFIG_F="configs"
-DBS_F="databases"
+SCRIPT_V="2.9.7"
 
 # TODO: otra opcion 'complete_site' para que intente restaurar archivos y base de un proyecto
 # TODO: otra opcion 'multi_sites' para que intente restaurar sitios que estan backupeados en dropbox
@@ -123,10 +14,18 @@ DBS_F="databases"
 #Restore Local?
 #$SFOLDER/tmp/backups/*.tar.gz
 
+source ${SFOLDER}/libs/commons.sh
+
 ### Checking some things
-if test -f /root/.broobe-utils-options ; then
-  source /root/.broobe-utils-options
+if [[ -z "${SFOLDER}" ]]; then
+  echo -e ${RED}" > Error: The script can only be runned by runner.sh! Exiting ..."${ENDCOLOR}
+  exit 0
 fi
+################################################################################
+
+SITES_F="sites"
+CONFIG_F="configs"
+DBS_F="databases"
 
 RESTORE_TYPES="${SITES_F} ${CONFIG_F} ${DBS_F}"
 
---
+++ utils/certbot_installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Autor: broobe. web + mobile development - https://broobe.com
+# Script Name: Broobe Utils Scripts
+# Version: 2.9.7
+################################################################################
+
+the_ppa=certbot
+
+if ! grep -q "^deb .*$the_ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+  echo -e ${GREEN}" > Adding ppa:certbot/certbot ..."${ENDCOLOR}
+  echo " > Adding ppa:certbot/certbot ..." >> $LOG
+    add-apt-repository ppa:certbot/certbot
+
+fi
+
+apt --yes update
+
+echo -e ${GREEN}" > Installing python3-certbot-dns-cloudflare and python3-certbot-nginx ..."${ENDCOLOR}
+echo " > Installing python3-certbot-dns-cloudflare and python3-certbot-nginx ..." >> $LOG
+
+apt install python3-certbot-dns-cloudflare python3-certbot-nginx
+
+echo " > DONE" >> $LOG
+echo -e ${GREEN}" > DONE"${ENDCOLOR}
