70aa6ecaeaf591277c01f8068e228c93f9a0e515
lpadula <leandro@broobe.com>
2019-08-23 00:15:50 -0300

    mejora en delete_project

--- delete_project.sh
+++ delete_project.sh
@@ -17,20 +17,32 @@ source ${SFOLDER}/libs/mysql_helper.sh
 
 ################################################################################
 
+# Folder where sites are hosted
 ask_folder_to_install_sites
 
 MENU_TITLE="PROJECT TO DELETE"
 directory_browser "${MENU_TITLE}" "${FOLDER_TO_INSTALL}"
 
+# Creating a tmp directory
 mkdir ${SFOLDER}/tmp-backup
 
+# Making a backup of project files
 echo -e ${CYAN}" > Making a backup ..."${ENDCOLOR}
 cp -r $filepath"/"$filename ${SFOLDER}/tmp-backup
 echo -e ${GREEN}" > Project files stored: ${SFOLDER}/tmp-backup"${ENDCOLOR}
 
-rm -R ${SFOLDER}/tmp-backup
+# Deleting project files
+rm -R $filepath"/"$filename
 echo -e ${GREEN}" > Project Files Deleted!"${ENDCOLOR}
 
+# Making a copy of nginx configuration file
+cp -r /etc/nginx/sites-available/${filename} ${SFOLDER}/tmp-backup
+
+# Deleting nginx configuration file
+rm /etc/nginx/sites-available/${filename}
+rm /etc/nginx/sites-enabled/${filename}
+
+# List databases
 mysql_databases_list
 CHOSEN_DB=$(whiptail --title "MYSQL DATABASES" --menu "Choose a Database to work with" 20 78 10 $(for x in ${DBS}; do echo "$x [DB]"; done) 3>&1 1>&2 2>&3)
 exitstatus=$?
@@ -42,8 +54,10 @@ fi
 
 # TODO: remove _STATE to make USER_DB
 
+# Making a database Backup
 mysql_database_export "${CHOSEN_DB}" "${filename}_DB.sql"
 
 #mysql_user_delete "${USER_DB}"
 
+# Deleting project database
 mysql_database_drop "${CHOSEN_DB}"
