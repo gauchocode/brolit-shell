702ca7f7adbd9ffd5fcd078bfec5477827bdf995
lpadula <leandro@broobe.com>
2019-08-29 23:33:10 -0300

    better mail notifications, wp-cli updated,  bugfix

--- libs/mail_notification_helper.sh
+++ libs/mail_notification_helper.sh
@@ -12,8 +12,8 @@ send_mail_notification() {
     # $1- ${EMAIL_SUBJECT}
     # $2- ${EMAIL_CONTENT}
 
-    EMAIL_SUBJECT=$1
-    EMAIL_CONTENT=$2
+    local EMAIL_SUBJECT=$1
+    local EMAIL_CONTENT=$2
 
     sendEmail -f ${SMTP_U} -t "${MAILA}" -u "${EMAIL_SUBJECT}" -o message-content-type=html -m "${EMAIL_CONTENT}" -s ${SMTP_SERVER}:${SMTP_PORT} -o tls=${SMTP_TLS} -xu ${SMTP_U} -xp ${SMTP_P}
 
@@ -25,9 +25,9 @@ mail_subject_status() {
     # $2 = ${STATUS_F}
     # $3 = ${OUTDATED}
 
-    STATUS_D=$1
-    STATUS_F=$2
-    OUTDATED=$3
+    local STATUS_D=$1
+    local STATUS_F=$2
+    local OUTDATED=$3
 
     if [ "${STATUS_D}" = "ERROR" ] || [ "${STATUS_F}" = "ERROR" ]; then
         STATUS="⛔ ERROR"
@@ -48,7 +48,7 @@ mail_subject_status() {
 
 remove_mail_notifications_files() {
 
-    echo " > Removing temp files ..." >>$LOG
+    echo "  Removing temp files ..." >>$LOG
     echo -e ${YELLOW}" > Removing temp files ..."${ENDCOLOR}
 
     # TODO: no siempre se crean estos archivos, entonces suele tirar un error, mejorar
@@ -61,16 +61,31 @@ mail_server_status_section() {
     # $1 - ${IP}
     # $2 - ${DISK_U}
 
-    IP=$1
-    DISK_U=$2
+    local IP=$1
+    local DISK_U=$2
 
-    SRV_HEADEROPEN='<div style="float:left;width:100%"><div style="font-size:14px;font-weight:bold;color:#FFF;float:left;font-family:Verdana,Helvetica,Arial;line-height:36px;background:#1DC6DF;padding:0 0 10px 10px;width:100%;height:30px">'
-    SRV_HEADERTEXT="Server Info"
+    # extract % to compare
+    local DISK_U_NS=$(echo ${DISK_U} | cut -f1 -d'%')
+
+    if [ "$DISK_U_NS" -gt "45" ]; then
+        SRV_COLOR='#fb2f2f'
+        SRV_STATUS='WARNING'
+        SRV_STATUS_ICON="⚠"
+    else
+        SRV_COLOR='#1DC6DF'
+        SRV_STATUS='OK'
+        SRV_STATUS_ICON="✅"
+    fi
+
+    SRV_HEADEROPEN_1='<div style="float:left;width:100%"><div style="font-size:14px;font-weight:bold;color:#FFF;float:left;font-family:Verdana,Helvetica,Arial;line-height:36px;background:'
+    SRV_HEADEROPEN_2=${SRV_COLOR}
+    SRV_HEADEROPEN_3=';padding:5px 0 10px 10px;width:100%;height:30px">'
+    SRV_HEADERTEXT="Server Status: ${SRV_STATUS} ${SRV_STATUS_ICON}"
     SRV_HEADERCLOSE='</div>'
-    SRV_HEADER=${SRV_HEADEROPEN}${SRV_HEADERTEXT}${SRV_HEADERCLOSE}
+    SRV_HEADER=${SRV_HEADEROPEN_1}${SRV_HEADEROPEN_2}${SRV_HEADEROPEN_3}${SRV_HEADERTEXT}${SRV_HEADERCLOSE}
 
     SRV_BODYOPEN='<div style="color:#000;font-size:12px;line-height:32px;float:left;font-family:Verdana,Helvetica,Arial;background:#D8D8D8;padding:10px;width:100%;">'
-    SRV_CONTENT="<b>Server IP: ${IP}</b><br /><b>Disk usage: ${DISK_U}</b>.<br />"
+    SRV_CONTENT="<b>Server IP: ${IP}</b><br /><b>Disk usage: ${DISK_U}</b><br />"
     SRV_BODYCLOSE='</div></div>'
     SRV_BODY=${SRV_BODYOPEN}${SRV_CONTENT}${SRV_BODYCLOSE}
 
@@ -83,7 +98,7 @@ mail_package_status_section() {
 
     # $1 - ${OUTDATED}
 
-    OUTDATED=$1
+    local OUTDATED=$1
 
     if [ "${OUTDATED}" = true ]; then
         PKG_COLOR='red'
@@ -94,13 +109,13 @@ mail_package_status_section() {
     fi
 
     PKG_HEADEROPEN1='<div style="float:left;width:100%"><div style="font-size:14px;font-weight:bold;color:#FFF;float:left;font-family:Verdana,Helvetica,Arial;line-height:36px;background:'
-    PKG_HEADEROPEN2=';padding:0 0 10px 10px;width:100%;height:30px">'
+    PKG_HEADEROPEN2=';padding:5px 0 10px 10px;width:100%;height:30px">'
     PKG_HEADEROPEN=${PKG_HEADEROPEN1}${PKG_COLOR}${PKG_HEADEROPEN2}
-    PKG_HEADERTEXT="Packages Status -> ${PKG_STATUS}"
+    PKG_HEADERTEXT="Packages Status: ${PKG_STATUS}"
     PKG_HEADERCLOSE='</div>'
 
-    PKG_BODYOPEN= $(mail_section_start)
-    PKG_BODYOPEN= $(mail_section_end)
+    PKG_BODYOPEN=$(mail_section_start)
+    PKG_BODYOPEN=$(mail_section_end)
 
     PKG_HEADER=$PKG_HEADEROPEN$PKG_HEADERTEXT$PKG_HEADERCLOSE
 
@@ -116,7 +131,7 @@ mail_package_section() {
 
     # $1 - ${PACKAGES}
 
-    PACKAGES=$1
+    local PACKAGES=$1
 
     OUTDATED=false
     echo "" >${BAKWP}/pkg-${NOW}.mail
@@ -138,12 +153,12 @@ mail_filesbackup_section() {
     # $3 - ${BACKUPED_LIST}
     # $4 - ${BK_FL_SIZES}
 
-    ERROR=$1
-    ERROR_TYPE=$2
-    BACKUPED_LIST=$3
-    BK_FL_SIZES=$4
+    local ERROR=$1
+    local ERROR_TYPE=$2
+    local BACKUPED_LIST=$3
+    local BK_FL_SIZES=$4
 
-    BK_TYPE="Files"
+    local BK_TYPE="Files"
 
     if [ "$ERROR" = true ]; then
         STATUS_ICON_F="💩"
@@ -178,9 +193,9 @@ mail_filesbackup_section() {
     fi
 
     HEADEROPEN1='<div style="float:left;width:100%"><div style="font-size:14px;font-weight:bold;color:#FFF;float:left;font-family:Verdana,Helvetica,Arial;line-height:36px;background:'
-    HEADEROPEN2=';padding:0 0 10px 10px;width:100%;height:30px">'
+    HEADEROPEN2=';padding:5px 0 10px 10px;width:100%;height:30px">'
     HEADEROPEN=$HEADEROPEN1$COLOR$HEADEROPEN2
-    HEADERTEXT="Files Backup -> ${STATUS_F} ${STATUS_ICON_F}"
+    HEADERTEXT="Files Backup: ${STATUS_F} ${STATUS_ICON_F}"
     HEADERCLOSE='</div>'
 
     BODYOPEN='<div style="color:#000;font-size:12px;line-height:32px;float:left;font-family:Verdana,Helvetica,Arial;background:#D8D8D8;padding:10px;width:100%;">'
@@ -205,10 +220,10 @@ mail_mysqlbackup_section() {
     # $3 - ${BACKUPEDLIST}
     # $4 - ${BK_DB_SIZES}
 
-    ERROR=$1
-    ERROR_TYPE=$2
-    BACKUPEDLIST=$3
-    BK_DB_SIZES=$4
+    local ERROR=$1
+    local ERROR_TYPE=$2
+    local BACKUPEDLIST=$3
+    local BK_DB_SIZES=$4
 
     BK_TYPE="Database"
 
@@ -242,9 +257,9 @@ mail_mysqlbackup_section() {
     fi
 
     HEADEROPEN1_D='<div style="float:left;width:100%"><div style="font-size:14px;font-weight:bold;color:#FFF;float:left;font-family:Verdana,Helvetica,Arial;line-height:36px;background:'
-    HEADEROPEN2_D=';padding:0 0 10px 10px;width:100%;height:30px">'
+    HEADEROPEN2_D=';padding:5px 0 10px 10px;width:100%;height:30px">'
     HEADEROPEN_D=${HEADEROPEN1_D}${COLOR_D}${HEADEROPEN2_D}
-    HEADERTEXT_D="Database Backup -> ${STATUS_D} ${STATUS_ICON_D}"
+    HEADERTEXT_D="Database Backup: ${STATUS_D} ${STATUS_ICON_D}"
     HEADERCLOSE_D='</div>'
 
     BODYOPEN_D='<div style="color:#000;font-size:12px;line-height:32px;float:left;font-family:Verdana,Helvetica,Arial;background:#D8D8D8;padding:10px 0 0 10px;width:100%;">'
@@ -277,7 +292,7 @@ mail_footer() {
 
     # $1 = ${SCRIPT_V}
 
-    SCRIPT_V=$1
+    local SCRIPT_V=$1
 
     FOOTEROPEN='<div style="font-size:10px;float:left;font-family:Verdana,Helvetica,Arial;text-align:right;padding-right:5px;width:100%;height:20px">'
     SCRIPTSTRING="Script Version: ${SCRIPT_V} by Broobe."
