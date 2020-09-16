3b70de08afbf23296df35e3c377e60fbce453d60
lpadula <leandro@broobe.com>
2020-09-16 14:44:42 -0300

    backup_helper bugfixing

--- updater.sh
+++ updater.sh
@@ -13,21 +13,18 @@ BRANCH="master"
 
 self_update() {
 
-    #cd "${SCRIPTPATH}"
-
     # Store credentials on first git pull
     git config --global credential.helper store
 
     git fetch
 
     [ -n "$(git diff --name-only "origin/${BRANCH}" "${SCRIPTFILE}")" ] && {
-        echo "Found a new version of me, updating myself..."
+        echo "Found a new version of LEMP Script Utils, updating ..."
         #git pull --force
         git checkout "${BRANCH}"
         git reset --hard origin/master
         git pull --ff-only --force
-        #echo "Running the new version..."
-        #cd -                                   # return to original working dir
+
         #exec "${SCRIPTNAME}" "${ARGS[@]}"
 
         exit 1
