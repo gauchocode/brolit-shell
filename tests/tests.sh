5231c097ef1a578a78c97ef6390285e0ab0ac4eb
lpadula <leandro@broobe.com>
2020-12-17 14:02:35 -0300

    test for wpcli-helper

--- tests/tests.sh
+++ tests/tests.sh
@@ -475,7 +475,34 @@ test_is_wp_project(){
 
 ####################### Tests for project_helper.sh #######################
 
+test_wpcli_helper_funtions() {
 
+    local project_domain
+
+    project_domain="test.domain.com"
+
+    # TODO: create db and user_db
+
+    # Create mock project
+    wpcli_core_install "${SITES}/${project_domain}"
+
+    # Tests
+    test_wpcli_get_wpcore_version "${SITES}/${project_domain}"
+
+    test_wpcli_get_db_prefix "${SITES}/${project_domain}"
+    
+    test_wpcli_change_tables_prefix "${SITES}/${project_domain}"
+
+    test_wpcli_get_db_prefix "${SITES}/${project_domain}"
+
+    # Deleting temp files
+    #rm -R "${SITES}/${project_domain}"
+
+}
+
+####################### Tests for project_helper.sh #######################
+
+# TODO
 
 ####################### Tests for cloudflare_helper #######################
 
@@ -572,13 +599,15 @@ test_cloudflare_clear_cache() {
 
 log_section "Running Tests Suite"
 
-test_display_functions
+#test_display_functions
 
 test_common_funtions
 
 test_mysql_helper
 
 test_cloudflare_funtions
+
+test_wordpress_helper_funtions
 
 ################################################################################
 # Uncomment to run specific function
