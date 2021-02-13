c3f6cc63fe032fdfcc1179f641e44a1a3107d8e2
lpadula <leandro@broobe.com>
2021-02-13 18:57:20 -0300

    test_nginx_helper_functions

---
+++ tests/test_nginx_helper.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Autor: BROOBE. web + mobile development - https://broobe.com
+# Version: 3.0.13
+#############################################################################
+
+function test_nginx_helper_functions() {
+
+    test_nginx_server_change_phpv
+
+}
+
+function test_nginx_server_change_phpv() {
+
+    local nginx_server_file
+
+    log_subsection "Test: test_nginx_server_change_phpv"
+
+    nginx_server_file="wordpress_single.conf"
+
+    cp "${SFOLDER}/config/nginx/sites-available/wordpress_single" "${SFOLDER}/tmp/${nginx_server_file}"
+
+    # First, we need to set a PHPV on file
+    php_set_version_on_config "7.2" "${SFOLDER}/tmp/${nginx_server_file}"
+
+    # Function to test
+    nginx_server_change_phpv "${SFOLDER}/tmp/${nginx_server_file}" "7.4"
+
+    # Get php version
+    current_php_v=$(nginx_server_get_current_phpv "${SFOLDER}/tmp/${nginx_server_file}")
+
+    current_php_v="7.4"
+    if [[ ${current_php_v } -eq 0 ]]; then
+        display --indent 6 --text "- test_nginx_server_change_phpv" --result "PASS" --color WHITE
+    else
+        display --indent 6 --text "- test_nginx_server_change_phpv" --result "FAIL" --color RED
+    fi
+
+}
