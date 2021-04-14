20d4f02ee72dc1111506e644dc62ce0c56bc9f2f
lpadula <leandro@broobe.com>
2021-04-14 10:59:52 -0300

    small fixes and more domain extensions

--- utils/project_manager.sh
+++ utils/project_manager.sh
@@ -3,15 +3,16 @@
 # Autor: BROOBE. web + mobile development - https://broobe.com
 # Version: 3.0.22
 ################################################################################
-#
-# TODO: check when add www.DOMAIN.com and then select other stage != prod
 
 function project_manager_menu() {
+
+  local installation_types
+  local project_type
 
   # Installation types
   installation_types="Laravel,PHP"
 
-  project_type=$(whiptail --title "INSTALLATION TYPE" --menu "Choose an Installation Type" 20 78 10 $(for x in ${installation_types}; do echo "$x [X]"; done) 3>&1 1>&2 2>&3)
+  project_type=$(whiptail --title "INSTALLATION TYPE" --menu "Choose an Installation Type" 20 78 10 "$(for x in ${installation_types}; do echo "$x [X]"; done)" 3>&1 1>&2 2>&3)
   exitstatus=$?
   if [[ ${exitstatus} -eq 0 ]]; then
 
