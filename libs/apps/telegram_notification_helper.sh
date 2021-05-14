3c87bed52900ac006a00527cc3ae74fa4ae7219d
lpadula <leandro@broobe.com>
2021-05-14 18:08:37 -0300

    small improvements

--- libs/apps/telegram_notification_helper.sh
+++ libs/apps/telegram_notification_helper.sh
@@ -26,6 +26,7 @@ function telegram_send_notification() {
 	local notif_url 
 	local display_mode
 
+	# Display mode
 	display_mode="HTML"
 	
 	# API timeout
@@ -41,25 +42,25 @@ function telegram_send_notification() {
 	fi
 	
 	# Notification text
-	notif_text="<b>${notification_title}:</b><pre>${notification_content}</pre>"
+	notif_text="<b>${notification_title}: </b><pre>${notification_content}</pre>"
 	
 	# Log
 	log_event "info" "Sending Telegram notification ..."
-	#log_event "debug" "Running: curl --silent --insecure --max-time ${timeout} --data chat_id=${telegram_user_id} --data disable_notification=${notif_sound} --data parse_mode=${display_mode} --data text=${notif_text} ${notif_url}"
 
 	# Telegram command
 	telegram_notif_response="$(curl --silent --insecure --max-time "${timeout}" --data chat_id="${telegram_user_id}" --data "disable_notification=${notif_sound}" --data "parse_mode=${display_mode}" --data "text=${notif_text}" "${notif_url}")"
 	
 	# Check Result
 	telegram_notif_result="$(echo "${telegram_notif_response}" | grep "ok" | cut -d ":" -f2 | cut -d "," -f1)"
-	if [[ "${telegram_notif_result}" = "true" ]]; then
-		# Log success
+	if [[ ${telegram_notif_result} == "true" ]]; then
+		# Log on success
 		log_event "info" "Telegram notification sent!"
 		display --indent 6 --text "- Sending Telegram notification" --result "DONE" --color GREEN
 	
 	else
-		# Log failure
+		# Log on failure
 		log_event "error" "Telegram notification error!"
+		log_event "debug" "Telegram api call: curl --silent --insecure --max-time ${timeout} --data chat_id=${telegram_user_id} --data disable_notification=${notif_sound} --data parse_mode=${display_mode} --data text=${notif_text} ${notif_url}"
 		log_event "debug" "Telegram notification result: ${telegram_notif_result}"
 		log_event "debug" "Telegram notification response: ${telegram_notif_response}"
 		display --indent 6 --text "- Sending Telegram notification" --result "FAIL" --color RED
