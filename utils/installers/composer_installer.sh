a55145bd8a451271ce8f5e12c5d9adb19bef3fe5
lpadula <leandro@broobe.com>
2021-05-27 21:18:16 -0300

    3.0.27

--- utils/installers/composer_installer.sh
+++ utils/installers/composer_installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Autor: BROOBE. web + mobile development - https://broobe.com
-# Version: 3.0.26
+# Version: 3.0.27
 ################################################################################
 #
 # Ref: https://linuxize.com/post/how-to-install-and-use-composer-on-ubuntu-20-04/
