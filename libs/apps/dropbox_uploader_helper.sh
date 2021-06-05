c4acb826b57f7e7fb33e8fd641c80372ad85df69
lpadula <leandro@broobe.com>
2021-06-05 12:20:09 -0300

    3.0.31

--- libs/apps/dropbox_uploader_helper.sh
+++ libs/apps/dropbox_uploader_helper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Autor: BROOBE. web + mobile development - https://broobe.com
-# Version: 3.0.30
+# Version: 3.0.31
 ################################################################################
 
 function dropbox_account_space() {
--- tests/test_wp_helper.sh
+++ tests/test_wp_helper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Autor: BROOBE. web + mobile development - https://broobe.com
-# Version: 3.0.30
+# Version: 3.0.31
 #############################################################################
 
 function test_wpcli_helper_funtions() {
