0f63865e4149425a974adf25997d919a92901803
lpadula <leandro@broobe.com>
2021-06-07 14:34:07 -0300

    3.0.33

--- libs/apps/mysql_helper.sh
+++ libs/apps/mysql_helper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Autor: BROOBE. web + mobile development - https://broobe.com
-# Version: 3.0.32
+# Version: 3.0.33
 #############################################################################
 
 function mysql_ask_user_db_scope() {
--- utils/aliases.sh
+++ utils/aliases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Autor: BROOBE. web + mobile development - https://broobe.com
-# Version: 3.0.32
+# Version: 3.0.33
 ################################################################################
 
 source ~/.broobe-utils-options
@@ -25,8 +25,8 @@ if [[ ${DROPBOX_ENABLE} == "true" && -f ${DPU_CONFIG_FILE} ]]; then
 fi
 
 # Version
-SCRIPT_VERSION="3.0.32"
-ALIASES_VERSION="3.0.32-051"
+SCRIPT_VERSION="3.0.33"
+ALIASES_VERSION="3.0.33-051"
 
 # Log
 timestamp="$(date +%Y%m%d_%H%M%S)"
