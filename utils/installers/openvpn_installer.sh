a5668149ad947d4bc3a9a964e606eaefc8e82151
lpadula <leandro@broobe.com>
2021-07-08 23:00:55 -0300

    initial support for openvpn

---
+++ utils/installers/openvpn_installer.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+#
+# Autor: BROOBE. web + mobile development - https://broobe.com
+# Version: 3.0.42
+################################################################################
+#
+#   Ref: https://tecadmin.net/install-openvpn-client-on-ubuntu/
+#
+################################################################################
+
+function openvpn_check_if_installed() {
+
+  local openvpn_installed
+  local openvpn
+
+  openvpn="$(command -v openvpn)"
+  if [[ ! -x "${openvpn}" ]]; then
+    openvpn_installed="false"
+
+  else
+    openvpn_installed="true"
+
+  fi
+
+  log_event "debug" "openvpn_installed=${openvpn_installed}" "false"
+
+  # Return
+  echo "${openvpn_installed}"
+
+}
+
+function openvpn_installer() {
+
+    log_subsection "OpenVPN Installer"
+
+    # Updating Repos
+    display --indent 6 --text "- Updating repositories"
+    apt-get --yes update -qq >/dev/null
+    clear_last_line
+    display --indent 6 --text "- Updating repositories" --result "DONE" --color GREEN
+
+    # Installing OpenVPN
+    display --indent 6 --text "- Installing OpenVPN"
+    log_event "info" "Installing openvpn" "false"
+
+    # apt command
+    apt-get --yes install openvpn -qq >/dev/null
+
+    # Log
+    clear_last_line
+    display --indent 6 --text "- Installing OpenVPN" --result "DONE" --color GREEN
+    log_event "info" "OpenVPN installation finished" "false"
+
+}
+
+function openvpn_purge() {
+
+  log_subsection "OpenVPN Installer"
+
+  # Log
+  display --indent 6 --text "- Removing OpenVPN"
+  log_event "info" "Removing OpenVPN ..." "false"
+
+  # apt command
+  apt-get --yes purge openvpn -qq >/dev/null
+
+  # Log
+  clear_last_line
+  display --indent 6 --text "- Removing OpenVPN" --result "DONE" --color GREEN
+  log_event "info" "openvpn removed" "false"
+
+}
+
+function openvpn_installer_menu() {
+
+  local openvpn_is_installed
+
+  openvpn_is_installed="$(openvpn_check_if_installed)"
+
+  if [[ ${openvpn_is_installed} == "false" ]]; then
+
+    openvpn_installer_title="OPENVPN INSTALLER"
+    openvpn_installer_message="Choose an option to run:"
+    openvpn_installer_options=(
+      "01)" "INSTALL OPENVPN"
+    )
+
+    chosen_openvpn_installer_options="$(whiptail --title "${openvpn_installer_title}" --menu "${openvpn_installer_message}" 20 78 10 "${openvpn_installer_options[@]}" 3>&1 1>&2 2>&3)"
+    exitstatus=$?
+    if [[ ${exitstatus} -eq 0 ]]; then
+
+      if [[ ${chosen_openvpn_installer_options} == *"01"* ]]; then
+
+        openvpn_installer
+
+      fi
+
+    fi
+
+  else
+
+    openvpn_installer_title="OPENVPN INSTALLER"
+    openvpn_installer_message="Choose an option to run:"
+    openvpn_installer_options=(
+      "01)" "UNINSTALL OPENVPN"
+    )
+
+    chosen_openvpn_installer_options="$(whiptail --title "${openvpn_installer_title}" --menu "${openvpn_installer_message}" 20 78 10 "${openvpn_installer_options[@]}" 3>&1 1>&2 2>&3)"
+    exitstatus=$?
+    if [[ ${exitstatus} -eq 0 ]]; then
+
+      if [[ ${chosen_openvpn_installer_options} == *"01"* ]]; then
+
+        openvpn_purge
+
+      fi
+
+    fi
+
+  fi
+
+}
