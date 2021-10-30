c9c9a50994604688fc3a7ca87811deba2961fd05
lpadula <leandro@broobe.com>
2021-10-30 15:33:44 -0300

    fix on test_json_helper_funtions

--- tests/test_json_helper.sh
+++ tests/test_json_helper.sh
@@ -24,15 +24,19 @@ function test_json_helper_funtions() {
         echo "FAILED"
     fi
 
-    MONIT_CONFIG_SERVICES="$(json_read_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services")"
+    #MONIT_CONFIG_SERVICES="$(json_read_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services")"
 
-    echo "MONIT_CONFIG_SERVICES=${MONIT_CONFIG_SERVICES}"
+    #echo "MONIT_CONFIG_SERVICES=${MONIT_CONFIG_SERVICES}"
 
     MONIT_CONFIG_SERVICES_REDIS="$(json_read_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services[].redis")"
 
     echo "MONIT_CONFIG_SERVICES_REDIS=${MONIT_CONFIG_SERVICES_REDIS}"
 
     json_write_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services[].redis" "enabled"
+
+    MONIT_CONFIG_SERVICES_REDIS="$(json_read_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services[].redis")"
+
+    echo "MONIT_CONFIG_SERVICES_REDIS=${MONIT_CONFIG_SERVICES_REDIS}"
 
     if [[ "${MONIT_CONFIG_SERVICES_REDIS}" == "enabled" ]]; then
         echo "PASSED"
@@ -43,6 +47,8 @@ function test_json_helper_funtions() {
     json_write_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services[]" "\"test\": \"disabled\""
 
     json_write_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services[].test" "enabled"
+
+    MONIT_CONFIG_SERVICES_TEST="$(json_read_field "${brolit_config}" "SUPPORT.monit[].config[].monit_services[].test")"
 
     if [[ "${MONIT_CONFIG_SERVICES_TEST}" == "enabled" ]]; then
         echo "PASSED"
