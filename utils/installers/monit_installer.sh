fc27545a552741cac18be67b0158b2e45832d8ec
lpadula <leandro@broobe.com>
2022-05-02 15:37:33 -0300

    improvements on monit and netdata installers

--- utils/installers/monit_installer.sh
+++ utils/installers/monit_installer.sh
@@ -107,6 +107,9 @@ function monit_configure() {
     local monit_pass="${PACKAGES_MONIT_CONFIG_HTTPD_PASS}"
     sed -i "s#MONIT_PASSWORD#${monit_pass}#" "/etc/monit/monitrc"
 
+    # Allow monit httpd port
+    firewall_allow "2812"
+
   fi
 
   if [[ ${PACKAGES_NETDATA_STATUS} == "enabled" ]]; then
