06d9b5ff8abb1689ae6aa39795f1598e2add810b
lpadula <leandro@broobe.com>
2022-08-27 19:36:25 +0100

    better discord_send_notification

--- libs/apps/discord_notification_helper.sh
+++ libs/apps/discord_notification_helper.sh
@@ -27,6 +27,29 @@ function discord_send_notification() {
     local notification_content="${2}"
     #local notification_type="${3}"
 
+    # Log
+    log_event "info" "Sending Discord notification ..." "false"
+    log_event "debug" "Running: ${CURL} -H \"Content-Type: application/json\" -X POST -d '{\"content\":\"'\"${notification_title} : ${notification_content}\"'\"}' \"${NOTIFICATION_DISCORD_WEBHOOK}\"" "false"
+
+    # Discord command
     ${CURL} -H "Content-Type: application/json" -X POST -d '{"content":"'"${notification_title} : ${notification_content}"'"}' "${NOTIFICATION_DISCORD_WEBHOOK}"
+
+    exitstatus=$?
+    if [[ ${exitstatus} -eq 0 ]]; then
+
+        # Log on success
+        log_event "info" "Discord notification sent!"
+        display --indent 6 --text "- Sending Discord notification" --result "DONE" --color GREEN
+
+        return 0
+
+    else
+        # Log on failure
+        log_event "error" "Discord notification error!" "false"
+        display --indent 6 --text "- Sending Discord notification" --result "FAIL" --color RED
+
+        return 1
+
+    fi
 
 }
