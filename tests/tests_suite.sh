d73a62df9d3cf9387ec74deac4004215346f6f20
Leandro <leandro.padula@gmail.com>
2022-08-29 19:18:38 +0100

    RUN BROLIT NOTIFICATION TESTS

--- tests/tests_suite.sh
+++ tests/tests_suite.sh
@@ -21,6 +21,7 @@ function tests_suite_menu() {
     "09)" "RUN OTHER TESTS"
     "10)" "RUN DISPLAY TESTS"
     "11)" "RUN DOCKER TESTS"
+    "12)" "RUN BROLIT NOTIFICATION TESTS"
   )
 
   chosen_tests_options=$(whiptail --title "TESTS SUITE" --menu " " 20 78 10 "${tests_options[@]}" 3>&1 1>&2 2>&3)
@@ -77,6 +78,10 @@ function tests_suite_menu() {
     fi
     if [[ ${chosen_tests_options} == *"11"* ]]; then
       test_docker_helper_functions
+
+    fi
+    if [[ ${chosen_tests_options} == *"12"* ]]; then
+      send_notification "${SERVER_NAME}" "This is a notification test message!" ""
 
     fi
 
