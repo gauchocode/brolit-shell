8ec742a5ba156d92a3c81f8e06e71ab74b3ab45d
lpadula <leandro@broobe.com>
2022-08-31 21:47:08 +0100

    new docker_project_install

--- libs/apps/docker_helper.sh
+++ libs/apps/docker_helper.sh
@@ -320,7 +320,7 @@ function docker_wordpress_install() {
     log_event "debug" "Setting PHPMYADMIN_DOMAIN=db.${project_domain}" "false"
     sed -ie "s|^PHPMYADMIN_DOMAIN=.*$|PHPMYADMIN_DOMAIN=db.${project_domain}|g" "${env_file}"
 
-    # TODO: replace 
+    # TODO: replace
     #### MYSQL_DATABASE=db_name
     #### MYSQL_USER=db_user
     #### MYSQL_PASSWORD=db_user_pass
@@ -458,5 +458,188 @@ function docker_project_files_import() {
         fi
 
     fi
+
+}
+
+### NEW NEW NEW NEW NEW NEW NEW
+
+function docker_project_install() {
+
+    local project_path
+
+    # PUT ON A NEW FUNCTION?
+
+    # Project Type
+    if [[ -z ${project_type} ]]; then
+        project_type="$(project_ask_type "")"
+        [[ $? -eq 1 ]] && return 1
+    fi
+
+    log_section "Project Installer (${project_type} on docker)"
+
+    # Project Domain
+    if [[ -z ${project_domain} ]]; then
+        project_domain="$(project_ask_domain "")"
+        [[ $? -eq 1 ]] && return 1
+    fi
+
+    # If ${dir_path} is empty, use default project path
+    [[ -z ${dir_path} ]] && dir_path="${PROJECTS_PATH}"
+
+    # Project Path
+    project_path="${dir_path}/${project_domain}"
+
+    # Project root domain
+    project_root_domain="$(domain_get_root "${project_domain}")"
+
+    # TODO: check when add www.DOMAIN.com and then select other stage != prod
+    if [[ -z ${project_stage} ]]; then
+
+        suggested_state="$(domain_get_subdomain_part "${project_domain}")"
+
+        # Project stage
+        project_stage="$(project_ask_stage "${suggested_state}")"
+        exitstatus=$?
+        if [[ ${exitstatus} -eq 1 ]]; then
+            # Log
+            log_event "info" "Operation cancelled!" "false"
+            display --indent 2 --text "- Asking project stage" --result SKIPPED --color YELLOW
+            return 1
+        fi
+
+    fi
+
+    if [[ -z ${project_name} ]]; then
+
+        possible_project_name="$(project_get_name_from_domain "${project_domain}")"
+
+        # Project Name
+        project_name="$(project_ask_name "${possible_project_name}")"
+        exitstatus=$?
+        if [[ ${exitstatus} -eq 1 ]]; then
+            # Log
+            log_event "info" "Operation cancelled!" "false"
+            display --indent 2 --text "- Asking project name" --result SKIPPED --color YELLOW
+            return 1
+        fi
+
+    fi
+
+    [[ ${project_domain} == "${project_root_domain}" ]] && project_domain="www.${project_domain}" && project_secondary_subdomain="${project_root_domain}"
+
+    case ${project_type} in
+
+    wordpress)
+
+        # Create project directory
+        mkdir -p "${project_path}"
+
+        # Copy docker-compose files
+        copy_files "${BROLIT_MAIN_DIR}/config/docker-compose/wordpress/production-stack-proxy/" "${project_path}"
+
+        # Replace .env vars
+        local wp_port=81
+        local project_database="${project_name}_${project_stage}"
+        local project_database_user="${project_name}_user"
+        local project_database_user_passw=$(openssl rand -hex 5)
+        local project_database_root_passw=$(openssl rand -hex 5)
+
+        ## PROJECT
+        sed -ie "s|^PROJECT_NAME=.*$|PROJECT_NAME=${project_name}|g" "${project_path}/.env"
+        sed -ie "s|^PROJECT_DOMAIN=.*$|PROJECT_DOMAIN=${project_domain}|g" "${project_path}/.env"
+        
+        ## WP
+        sed -ie "s|^WP_PORT=.*$|WP_PORT=${wp_port}|g" "${project_path}/.env"
+
+        ##  MYSQL
+        sed -ie "s|^MYSQL_DATABASE=.*$|MYSQL_DATABASE=${project_database}|g" "${project_path}/.env"
+        sed -ie "s|^MYSQL_USER=.*$|MYSQL_USER=${project_database_user}|g" "${project_path}/.env"
+        sed -ie "s|^MYSQL_PASSWORD=.*$|MYSQL_PASSWORD=${project_database_user_passw}|g" "${project_path}/.env"
+        sed -ie "s|^MYSQL_ROOT_PASSWORD=.*$|MYSQL_ROOT_PASSWORD=${project_database_root_passw}|g" "${project_path}/.env"
+
+        # Execute docker-compose commands
+        docker-compose pull
+        docker-compose up -d
+
+        # Execute function
+        #wordpress_project_installer "${project_path}" "${project_domain}" "${project_name}" "${project_stage}" "${project_root_domain}" "${project_install_mode}"
+
+        ;;
+
+        #    laravel)
+        #        # Execute function
+        #        # laravel_project_installer "${project_path}" "${project_domain}" "${project_name}" "${project_stage}" "${project_root_domain}"
+        #        # log_event "warning" "Laravel installer should be implemented soon, trying to install like pure php project ..."
+        #        php_project_installer "${project_path}" "${project_domain}" "${project_name}" "${project_stage}" "${project_root_domain}"
+        #
+        #        ;;
+        #
+        #    php)
+        #
+        #        php_project_installer "${project_path}" "${project_domain}" "${project_name}" "${project_stage}" "${project_root_domain}"
+        #
+        #        ;;
+        #
+    *)
+        log_event "error" "Project type '${project_type}' unkwnown, aborting ..." "false"
+        return 1
+        ;;
+
+    esac
+
+    ### NEW ###
+
+    # Project domain configuration (webserver+certbot+DNS)
+    https_enable="$(project_update_domain_config "${project_domain}" "${project_type}" "")"
+
+    # Startup Script for WordPress installation
+    if [[ ${https_enable} == "true" ]]; then
+        project_site_url="https://${project_domain}"
+    else
+        project_site_url="http://${project_domain}"
+    fi
+
+    [[ ${EXEC_TYPE} == "default" && ${project_type} == "wordpress" ]] && wpcli_run_startup_script "${project_path}" "${project_site_url}"
+
+    # Post-restore/install tasks
+    project_post_install_tasks "${project_path}" "${project_type}" "${project_name}" "${project_stage}" "${database_user_passw}" "" ""
+
+    # TODO: refactor this
+    # Cert config files
+    cert_path=""
+    if [[ -d "/etc/letsencrypt/live/${project_domain}" ]]; then
+        cert_path="/etc/letsencrypt/live/${project_domain}"
+    else
+        if [[ -d "/etc/letsencrypt/live/www.${project_domain}" ]]; then
+            cert_path="/etc/letsencrypt/live/www.${project_domain}"
+        fi
+    fi
+
+    # Create project config file
+    # Arguments:
+    #  $1 = ${project_path}
+    #  $2 = ${project_name}
+    #  $3 = ${project_stage}
+    #  $4 = ${project_type}
+    #  $5 = ${project_db_status}
+    #  $6 = ${project_db_engine}
+    #  $7 = ${project_db_name}
+    #  $8 = ${project_db_host}
+    #  $9 = ${project_db_user}
+    #  $10 = ${project_db_pass}
+    #  $11 = ${project_prymary_subdomain}
+    #  $12 = ${project_secondary_subdomains}
+    #  $13 = ${project_override_nginx_conf}
+    #  $14 = ${project_use_http2}
+    #  $15 = ${project_certbot_mode}
+    project_update_brolit_config "${project_path}" "${project_name}" "${project_stage}" "${project_type} " "enabled" "mysql" "${database_name}" "localhost" "${database_user}" "${database_user_passw}" "${project_domain}" "${project_secondary_subdomain}" "/etc/nginx/sites-available/${project_domain}" "" "${cert_path}"
+
+    # Log
+    log_event "info" "New ${project_type} project installation for '${project_domain}' finished ok." "false"
+    display --indent 6 --text "- ${project_type} project installation" --result "DONE" --color GREEN
+    display --indent 8 --text "for domain ${project_domain}"
+
+    # Send notification
+    send_notification "✅ ${SERVER_NAME}" "New ${project_type} project installation for '${project_domain}' finished ok!" ""
 
 }
