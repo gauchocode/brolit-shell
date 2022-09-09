844710135b23c90c8c02f7407e22521c8364ad47
Leandro <leandro.padula@gmail.com>
2022-09-09 16:27:08 +0100

    refactor on restore functions

--- utils/project_manager.sh
+++ utils/project_manager.sh
@@ -555,7 +555,7 @@ function project_install_tasks_handler() {
 
   *)
 
-    log_event "error" "INVALID PROJECT INSTALLER TYPE: ${project_install_type}" "true"
+    log_event "error" "INVALID PROJECT INSTALL TYPE: ${project_install_type}" "true"
 
     exit
     ;;
