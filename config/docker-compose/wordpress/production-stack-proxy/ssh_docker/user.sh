e0b8844b648c45c06d0527c9122151e52138d083
lpadula <leandro@broobe.com>
2025-12-16 10:45:21 -0300

    docker compose config fixes

--- config/docker-compose/wordpress/production-stack-proxy/ssh_docker/user.sh
+++ config/docker-compose/wordpress/production-stack-proxy/ssh_docker/user.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 set -e
- 
-printf "\n\033[0;44m---> Creating SSH master user.\033[0m\n"
- 
-useradd -m -d /home/${SSH_MASTER_USER} -G ssh ${SSH_MASTER_USER} -s /bin/bash
-echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
-echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_MASTER_USER}/.profile
- 
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/rm" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/mkdir" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/bin/chown" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/useradd" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/deluser" >> /etc/sudoers
-echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/chpasswd" >> /etc/sudoers
- 
-addgroup sftp
 
-usermod -aG www-data ${SSH_MASTER_PASS}
- 
-exec "$@"
+printf "\n\033[0;44m---> Creating SSH master user.\033[0m\n"
+
+# Crear grupos si no existen
+groupadd -f ssh
+groupadd -f sftp
+
+# Crear usuario
+useradd -m \
+  -d /home/${SSH_MASTER_USER} \
+  -s /bin/bash \
+  -G ssh \
+  ${SSH_MASTER_USER}
+
+# Setear password
+echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
+
+# Perfil PATH
+echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_MASTER_USER}/.profile
+chown ${SSH_MASTER_USER}:${SSH_MASTER_USER} /home/${SSH_MASTER_USER}/.profile
+
+# Sudo sin romper sudoers
+cat <<EOF > /etc/sudoers.d/${SSH_MASTER_USER}
+${SSH_MASTER_USER} ALL=NOPASSWD:/bin/rm
+${SSH_MASTER_USER} ALL=NOPASSWD:/bin/mkdir
+${SSH_MASTER_USER} ALL=NOPASSWD:/bin/chown
+${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/useradd
+${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/deluser
+${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/chpasswd
+EOF
+
+chmod 0440 /etc/sudoers.d/${SSH_MASTER_USER}
+
+# Agregar a www-data (CORRECTO)
+usermod -aG www-data ${SSH_MASTER_USER}
