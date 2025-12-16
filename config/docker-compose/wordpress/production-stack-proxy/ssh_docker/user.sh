95c574f95977a8c318e24462ee2dfda82dee6ecf
Leandro Padula <leandro@broobe.com>
2025-12-16 12:00:05 -0300

    fix ssh permissions

--- config/docker-compose/wordpress/production-stack-proxy/ssh_docker/user.sh
+++ config/docker-compose/wordpress/production-stack-proxy/ssh_docker/user.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 set -e
 
-printf "\n\033[0;44m---> Creating SSH master user.\033[0m\n"
+echo "---> Creating SSH alias user"
 
-# Crear grupos si no existen
-groupadd -f ssh
-groupadd -f sftp
+# Asegurar shell para www-data
+usermod -s /bin/bash www-data
 
-# Crear usuario
-useradd -m \
-  -d /home/${SSH_MASTER_USER} \
-  -s /bin/bash \
-  -G ssh \
-  ${SSH_MASTER_USER}
+# Crear alias (mismo UID/GID)
+if ! id "${SSH_MASTER_USER}" >/dev/null 2>&1; then
+  useradd \
+    --non-unique \
+    -u 33 \
+    -g 33 \
+    -M \
+    -s /bin/bash \
+    "${SSH_MASTER_USER}"
+fi
 
-# Setear password
+# Home propio (solo para comodidad)
+if [ ! -d /home/${SSH_MASTER_USER} ]; then
+  mkdir -p /home/${SSH_MASTER_USER}
+  chown 33:33 /home/${SSH_MASTER_USER}
+  chmod 755 /home/${SSH_MASTER_USER}
+fi
+
+# Password del alias
 echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
-
-# Perfil PATH
-echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${SSH_MASTER_USER}/.profile
-chown ${SSH_MASTER_USER}:${SSH_MASTER_USER} /home/${SSH_MASTER_USER}/.profile
-
-# Sudo sin romper sudoers
-cat <<EOF > /etc/sudoers.d/${SSH_MASTER_USER}
-${SSH_MASTER_USER} ALL=NOPASSWD:/bin/rm
-${SSH_MASTER_USER} ALL=NOPASSWD:/bin/mkdir
-${SSH_MASTER_USER} ALL=NOPASSWD:/bin/chown
-${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/useradd
-${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/deluser
-${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/chpasswd
-EOF
-
-chmod 0440 /etc/sudoers.d/${SSH_MASTER_USER}
-
-# Agregar a www-data (CORRECTO)
-usermod -aG www-data ${SSH_MASTER_USER}
