1122df3222b454e794b02403414f70dc72d812b7
lpadula <leandro@broobe.com>
2025-12-30 16:38:43 -0300

    small docker and dtop support improvements

--- utils/installers/docker_installer.sh
+++ utils/installers/docker_installer.sh
@@ -65,6 +65,8 @@ function docker_installer() {
 
 function docker_purge() {
 
+    log_subsection "Docker CE Uninstaller"
+
     # Remove docker and dependencies
     for pkg in docker docker-ce docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
         package_purge ${pkg}
