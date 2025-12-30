a4684243110f5238a3e585effe33e03267ebae81
Leandro Padula <leandro@broobe.com>
2025-12-30 11:40:40 -0300

    more docker configs fixes

--- config/docker-compose/wordpress/production-stack-proxy/ssh_docker/user.sh
+++ config/docker-compose/wordpress/production-stack-proxy/ssh_docker/user.sh
@@ -5,20 +5,26 @@ echo "---> Creating SSH alias user"
 
 usermod -s /bin/bash www-data
 
-if ! id "${SSH_MASTER_USER}" >/dev/null 2>&1; then
-  useradd \
-    --non-unique \
-    -u 33 \
-    -g 33 \
-    -M \
-    -s /bin/bash \
-    "${SSH_MASTER_USER}"
+if [ -n "${SSH_MASTER_USER}" ]; then
+  if ! id "${SSH_MASTER_USER}" >/dev/null 2>&1; then
+    useradd \
+      --non-unique \
+      -u 33 \
+      -g 33 \
+      -M \
+      -s /bin/bash \
+      "${SSH_MASTER_USER}"
+  fi
+
+  if [ ! -d /home/${SSH_MASTER_USER} ]; then
+    mkdir -p /home/${SSH_MASTER_USER}
+    chown 33:33 /home/${SSH_MASTER_USER}
+    chmod 755 /home/${SSH_MASTER_USER}
+  fi
+
+  echo "SSH user created: ${SSH_MASTER_USER}"
+else
+  echo "WARNING: SSH_MASTER_USER not set, using default www-data"
 fi
 
-if [ ! -d /home/${SSH_MASTER_USER} ]; then
-  mkdir -p /home/${SSH_MASTER_USER}
-  chown 33:33 /home/${SSH_MASTER_USER}
-  chmod 755 /home/${SSH_MASTER_USER}
-fi
-
-echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
+# Password is now set in entrypoint.sh at runtime, not during build
