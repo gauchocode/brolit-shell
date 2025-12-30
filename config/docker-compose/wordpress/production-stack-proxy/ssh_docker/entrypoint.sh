bbc98ce4738ea0f938e91ff4a1d85379e16bde3c
lpadula <leandro@broobe.com>
2025-12-30 11:40:40 -0300

    more docker configs fixes

--- config/docker-compose/wordpress/production-stack-proxy/ssh_docker/entrypoint.sh
+++ config/docker-compose/wordpress/production-stack-proxy/ssh_docker/entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
- 
-printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
- 
+
+printf "\n\033[0;44m---> Configuring SSH user and starting server.\033[0m\n"
+
+# Set password at runtime (not build time)
+if [ -n "${SSH_MASTER_USER}" ] && [ -n "${SSH_MASTER_PASS}" ]; then
+    echo "${SSH_MASTER_USER}:${SSH_MASTER_PASS}" | chpasswd
+    echo "SSH user password configured for: ${SSH_MASTER_USER}"
+else
+    echo "WARNING: SSH_MASTER_USER or SSH_MASTER_PASS not set!"
+fi
+
+# Start SSH server
 service ssh start
 service ssh status
- 
+
 exec "$@"
